1f6c32d5557ce7aaf00682823854711a13689127
Brendan Darnell <brendan_darnell@hotmail.com>
2018-08-07 05:33:29 -0600

    Add second ajax request and an event handler to index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const NPS_URL= 'https://api.nps.gov/api/v1/parks';
 
 const NPS_KEY= 'fmTMScXlzTUKcMdt89Fmnyhl81mqm2xxxZH8Yppq';
 
+const HIKING_URL= 'https://www.hikingproject.com/data/get-trails';
+
+const HIKING_KEY= '200327700-d3521df4d4b42d0d77bf4b4d0ba20637';
+
+
 function getParkData(parkName){
   query = {
     q: parkName,
@@ -9,6 +14,7 @@ function getParkData(parkName){
   }
   return $.getJSON(NPS_URL,query);    
 }
+
 
 function createParkObject(parks){
   let parkObject=''; 
@@ -20,11 +26,13 @@ function createParkObject(parks){
   return (parkObject); 
 };
 
+
 function getLatLon(parkObject){
   let latLon = parkObject.latLong;
   latLon=latLon.split(',');
   return latLon;
 }
+
 
 function getTrailData(latLon){
   lat= latLon[0].trim().slice(4);
@@ -33,3 +41,34 @@ function getTrailData(latLon){
   console.log(lat);
   console.log(lon);
 }
+
+
+function getTrailData(latLon){
+  lat= latLon[0].trim().slice(4);
+  lon= latLon[1].trim().slice(5);
+  console.log(latLon);
+  console.log(lat);
+  console.log(lon); 
+  query={
+    'lat': lat,
+    'lon': lon,
+    key: HIKING_KEY,
+  }
+  
+  return $.getJSON(HIKING_URL,query);
+}
+
+
+function handleUserSearch(){
+  $('button').on('click',function(event){
+    event.preventDefault();
+    getParkData('acadia')
+      .then(createParkObject)
+      .then(getLatLon)
+      .then(getTrailData)
+      .done(data=>{console.log(data);});
+  });
+}
+
+
+$(handleUserSearch)
